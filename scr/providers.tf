3753cdd1cc8849bb9c1f3e78d4a2c6c7d87b81dd
Maybe-R <rita.flid@yandex.ru>
2025-07-08 16:09:34 +0300

    Update providers.tf

--- scr/providers.tf
+++ scr/providers.tf
@@ -11,6 +11,6 @@ provider "yandex" {
   # token     = var.token
   cloud_id                 = "b1gk34aj8huam4c6qc65"
   folder_id                = "b1groq7lioh1rbih3b87"
-  zone                     = ru-central1-a
+  zone                     = "ru-central1-a"
   service_account_key_file = file("~/.authorized_key.json")
 }
